a4a0ab03d341987edabf0b70140f69f6a678ef91
Zalman Izak <thezalmanian@gmail.com>
2023-01-17 16:28:31 -0800

    Add project files.

---
+++ E-CommerceSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_CommerceSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
