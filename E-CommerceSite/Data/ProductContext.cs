cf5ae00133f3cff48c87afd55ceceedb94a9eaec
Zalman Izak <thezalmanian@gmail.com>
2023-01-25 21:03:21 -0800

    Added DB Context class

---
+++ E-CommerceSite/Data/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using E_CommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_CommerceSite.Data
+{
+    public class ProductContext: DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
