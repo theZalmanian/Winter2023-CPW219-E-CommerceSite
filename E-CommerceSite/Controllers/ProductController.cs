0b4581620107a083f3ec1ff033ebba04bad6e562
Zalman Izak <thezalmanian@gmail.com>
2023-02-02 03:44:20 -0800

    Implemented adding Product to DB (Create) - Displays success message to user

--- E-CommerceSite/Controllers/ProductController.cs
+++ E-CommerceSite/Controllers/ProductController.cs
@@ -1,13 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_CommerceSite.Data;
+using E_CommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace E_CommerceSite.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly ProductContext dbContext;
+
+        public ProductController(ProductContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product currProduct)
+        {
+            // If all data is valid
+            if(ModelState.IsValid)
+            {
+                // Prepare INSERT Statement
+                dbContext.Products.Add(currProduct);
+
+                // Execute query
+                dbContext.SaveChanges();
+
+                // Output success message
+                ViewData["Message"] = $"{currProduct.ProductName} was added successfully";
+            }
+
+            // If all Product data not valid
+            return View(currProduct);
         }
     }
 }
