2ae5a1f6d97759003537efa11429042b5cb7c6e8
Zalman Izak <thezalmanian@gmail.com>
2023-02-21 01:55:20 -0800

    Added Member's table to DB using EF Core code first migrations

---
+++ E-CommerceSite/Migrations/20230221095338_AddedMember.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerceSite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedMember : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    MemberID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MemberEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MemberPassword = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MemberUsername = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MemberPhone = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.MemberID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Members");
+        }
+    }
+}
