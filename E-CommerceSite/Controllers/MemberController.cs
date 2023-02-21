4b2d7b7ee038bc0f2c687c1c4767124a30755de5
Zalman Izak <thezalmanian@gmail.com>
2023-02-21 03:09:46 -0800

    Created Members controller

---
+++ E-CommerceSite/Controllers/MemberController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_CommerceSite.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
