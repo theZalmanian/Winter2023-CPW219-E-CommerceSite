8f8848400d71e0a7015a5e121d93b3ecf65f72cf
Zalman Izak <thezalmanian@gmail.com>
2023-02-27 00:45:26 -0800

    Added Product cart view model

--- E-CommerceSite/Models/Product.cs
+++ E-CommerceSite/Models/Product.cs
@@ -3,7 +3,7 @@
 namespace E_CommerceSite.Models
 {
     /// <summary>
-    /// Represent's a product currently being sold
+    /// Represent's a Product currently being sold
     /// </summary>
     public class Product
     {
@@ -24,6 +24,28 @@ namespace E_CommerceSite.Models
         /// </summary>
         [Required]
         [Range(0, double.MaxValue)]
+        public double ProductPrice { get; set; }
+    }
+
+    /// <summary>
+    /// Represent's an individual Product that has been added 
+    /// to the current user's "shopping cart" cookie
+    /// </summary>
+    public class ProductCartViewModel
+    {
+        /// <summary>
+        /// The Product's unique identifier
+        /// </summary>
+        public int ProductID { get; set; }
+
+        /// <summary>
+        /// The Product's name
+        /// </summary>
+        public string ProductName { get; set; }
+
+        /// <summary>
+        /// The Product's sales price
+        /// </summary>
         public double ProductPrice { get; set; }
     }
 }
